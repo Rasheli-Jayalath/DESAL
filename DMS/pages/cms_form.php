9047eb902fd58e0f289af546c556a0b1427d1c5e
Rasheli-Jayalath <16.rasheli@gmail.com>
2021-11-11 16:05:39 +0530

    Updated -  cms, upload report add file

--- DMS/pages/cms_form.php
+++ DMS/pages/cms_form.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/table-styling.css" rel="stylesheet">
+
+
+    <title>DMS</title>
+</head>
+
+<body>
+
+
 <?php
 include("./fckeditor/fckeditor.php");
 $cms_path=CMS_PATH;
@@ -144,8 +168,9 @@ function doDeleteNewsFile(cms_cd,image,name) {
 		
 	}
 </script>
-<div id="wrapperPRight">
-		<div id="pageContentName" class="shadowWhite"><?php echo ($mode == "U") ? 'Edit CMS Page' : 'Add CMS Page';?></div>
+
+<h4 class="semibold"  style="text-align: center; margin: auto; margin-bottom: 20px; margin-top: 25PX;"><?php echo ($mode == "U") ? 'Edit CMS Page' : 'Add CMS Page';?></h4>
+<div id="wrapperPRight" class="container" style="margin-top: 20px; margin-bottom: 50px;  border-radius: 15px; border: 2px solid #dfdfdf;padding: 20px; ">
 		<div id="pageContentRight">
 		</div>
 		<div class="clear"></div>
@@ -154,39 +179,78 @@ function doDeleteNewsFile(cms_cd,image,name) {
 		
 		<div id="tableContainer">
 		<div class="clear"></div>			
-	  	    <form name="frmCms" id="frmCms" action="" method="post" onSubmit="return frmValidate(this);" enctype="multipart/form-data">
+	  	    <form name="frmCms" id="frmCms" action="" method="post" onSubmit="return frmValidate(this);" enctype="multipart/form-data" class="form-inline" >
 			
 			<input type="hidden" name="mode" id="mode" value="<?php echo $mode;?>" />
         	<input type="hidden" name="cms_cd" id="cms_cd" value="<?php echo $cms_cd;?>" />
-       		<div class="formfield b shadowWhite"><?php echo 'Title';?>:</div>
-			<div class="formvalue"><input class="rr_input" type="text" name="title1" id="title1" value="<?php echo $title;?>" style="width:485px" /></div>
-			<div class="clear"></div>
-			<div class="formfield b shadowWhite"><?php echo 'Details';?>:</div>
-			<div class="formvalue"> 
-			  <?php
-			  $oFCKeditor = new FCKeditor('details') ;
-			  $oFCKeditor->BasePath   = SITE_URL.'fckeditor/';
-			  $oFCKeditor->Width      = "506px";
-			  $oFCKeditor->Height     = "250";
-			  $oFCKeditor->ToolbarSet = "Basic";
-			  $oFCKeditor->Value     = stripslashes($details);      
-			  $oFCKeditor->Create( );
-			  ?>
+
+
+		<div class="row" style="margin-top: 10px;">
+
+            <div class="col-md-4" style="text-align: right; margin: auto; font-size: small;">
+              <label  class="sr-only bold">Title</label>
+            </div>
+
+            <div class=" col-md-4 regular" style="text-align: left; margin: auto;">
+                <input class="form-control commontextsize" type="text" name="title1" id="title1" value="<?php echo $title;?>" placeholder="Category Name">
+            </div>
+
+            <div class="col-md-4">
+            </div>
+
+        </div>
+
+
+		<div class="row" style="margin-top: 10px;">
+
+            <div class="col-md-4" style="text-align: right; margin: auto; font-size: small;">
+              <label  class="sr-only bold">Details</label>
+            </div>
+
+            <div class=" col-md-4 regular" style="text-align: left; margin: auto;">
+					<div class="formvalue"> 
+						<?php
+							$oFCKeditor = new FCKeditor('details') ;
+							$oFCKeditor->BasePath   = SITE_URL.'fckeditor/';
+							$oFCKeditor->Width      = "506px";
+							$oFCKeditor->Height     = "250";
+							$oFCKeditor->ToolbarSet = "Basic";
+							$oFCKeditor->Value     = stripslashes($details);      
+							$oFCKeditor->Create( );
+						?>
+					</div>
+            </div>
+
+            <div class="col-md-4"></div>
+
+        </div>
+
+		<div class="row" style="margin-top: 10px;">
+
+            <div class="col-md-4" style="text-align: right; margin: auto; font-size: small;">
+              <label  class="sr-only bold">Upload Image</label>
+            </div>
+
+            <div class=" col-md-4 regular" style="text-align: left; margin: auto;">
+			<input class="custom-file form-control commontextsize btn-primary" type="file" name="cmsfile" id="cmsfile" />
+            <input type="hidden" name="old_cms_file" value="<?php echo $cmsfile;?>" /><span class="text-success text-sm">Image width should be atleast 720px</span>
+            </div>
+
+            <div class="col-md-4">
+            </div>
+
+        </div>
+		
+
+			<div class="d-flex justify-content-center mt-4" style="margin:auto; ">
+				<button type="submit" class="btn btn-success mx-2"  value="<?php echo ($mode == "U") ? " Update " : " Save ";?>" >   <i class="bi bi-arrow-bar-up"    style="margin-right: 10px;"></i>Save     </button>
+				<button type="submit" class="btn btn-danger mx-2"   value=" Cancel " onclick="javascript: history.back(-1);"     >   <i class="bi bi-x-circle"        style="margin-right: 10px;"></i>Cancel   </button>
 			</div>
-			<div class="clear"></div>
-			<div class="formfield b shadowWhite"><?php echo 'Upload Image';?>:</div>
-			<div class="formvalue" style="width:250px">
-			<input type="file" name="cmsfile" id="cmsfile" />
-            <input type="hidden" name="old_cms_file" value="<?php echo $cmsfile;?>" /><span style="color:green">Image width should be atleast 720px</span>
-			</div>
-			
-			<div class="clear"></div>
-			<div id="submit" style="margin-left:164px;"><input type="submit" class="SubmitButton" value="<?php echo ($mode == "U") ? " Update " : " Save ";?>" /></div>
-			<div id="submit2">
-			<input type="button" class="SubmitButton" value=" Cancel " onclick="javascript: history.back(-1);" />
-			</div>
-			<div class="clear"></div>
+
 		    </form>
-			<div class="clear"></div>
+		
   	    </div>
 	</div>
+
+</body>
+</html>
